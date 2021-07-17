8aa11eefb92b5b83414f0eeabe38aba54396ca82
Beep <beepnboopn@gmail.com>
2021-07-17 18:56:41 -0400

    Initial changes

--- config/config-example.js
+++ config/config-example.js
@@ -1,5 +1,5 @@
 exports.routes = {
-    root: 'pokemonshowdown.com',
-    client: 'play.pokemonshowdown.com',
+    root: 'neodrafttest',
+    client: 'neodraft.herokuapp.com',
     replays: 'replay.pokemonshowdown.com',
 };
